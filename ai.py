c5377a9e70fba6febac3aaf284e973dc7f854872
Dana Owens <dlo49@cornell.edu>
2021-05-19 12:15:52 -0400

    min/max restruct

--- ai.py
+++ ai.py
@@ -1,41 +1,6 @@
 import dotbox
 
 
-# def max_alpha_beta(alpha, beta):
-#     maxv = -2
-#     px = None
-#     py = None
-
-#     result = is_end()
-
-#     if result == 'X':
-#         return (-1, 0, 0)
-#     elif result == 'O':
-#         return (1, 0, 0)
-#     elif result == '.':
-#         return (0, 0, 0)
-
-#     for i in range(0, 3):
-#         for j in range(0, 3):
-#             if self.current_state[i][j] == '.':
-#                 self.current_state[i][j] = 'O'
-#                 (m, min_i, in_j) = self.min_alpha_beta(alpha, beta)
-#                 if m > maxv:
-#                     maxv = m
-#                     px = i
-#                     py = j
-#                 self.current_state[i][j] = '.'
-
-#                 # Next two ifs in Max and Min are the only difference between regular algorithm and minimax
-#                 if maxv >= beta:
-#                     return (maxv, px, py)
-
-#                 if maxv > alpha:
-#                     alpha = maxv
-
-#     return (maxv, px, py)
-
-
 def maximize(gameState, players, alpha, beta):
     max_value = -2
     best_move = None
@@ -64,41 +29,6 @@ def maximize(gameState, players, alpha, beta):
         players.switch_player()
         return (ngameState, players, max_value, best_move)
 
-
-#  def min_alpha_beta(self, alpha, beta):
-
-#         minv = 2
-
-#         qx = None
-#         qy = None
-
-#         result = self.is_end()
-
-#         if result == 'X':
-#             return (-1, 0, 0)
-#         elif result == 'O':
-#             return (1, 0, 0)
-#         elif result == '.':
-#             return (0, 0, 0)
-
-#         for i in range(0, 3):
-#             for j in range(0, 3):
-#                 if self.current_state[i][j] == '.':
-#                     self.current_state[i][j] = 'X'
-#                     (m, max_i, max_j) = self.max_alpha_beta(alpha, beta)
-#                     if m < minv:
-#                         minv = m
-#                         qx = i
-#                         qy = j
-#                     self.current_state[i][j] = '.'
-
-#                     if minv <= alpha:
-#                         return (minv, qx, qy)
-
-#                     if minv < beta:
-#                         beta = minv
-
-#         return (minv, qx, qy)
 
 def minimize(gameState,
              players, alpha, beta):
