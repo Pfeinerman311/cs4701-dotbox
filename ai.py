fcdb1fc0aa7e79573902291b703956147177f658
Dana Owens <dlo49@cornell.edu>
2021-05-22 11:54:13 -0400

    ai changes

--- ai.py
+++ ai.py
@@ -11,22 +11,28 @@ def maximize(gameState, players, alpha, beta):
 
     for m in gameState.get_valid_moves():
         line = dotbox.Line(m, players.get_current_player())
-        ngameState = gameState.test_move(line)
+        scored, ngameState = gameState.test_move(line)
 
-        (x, y, v, min_move) = minimize(
-            ngameState, players, alpha, beta)
+        if scored:
+            (x, y, v, min_move) = maximize(
+                ngameState, players, alpha, beta)
+        else:
+            players.switch_player()
+            (x, y, v, min_move) = minimize(
+                ngameState, players, alpha, beta)
 
         if v > max_value:
             max_value = v
             best_move = m
 
         if max_value >= beta:
-            players.switch_player()
+            # players.switch_player()
             return (ngameState,  players, max_value, best_move)
 
         if max_value > alpha:
             alpha = max_value
-        players.switch_player()
+
+        # players.switch_player()
         return (ngameState, players, max_value, best_move)
 
 
@@ -41,22 +47,28 @@ def minimize(gameState,
     for m in gameState.get_valid_moves():
 
         line = dotbox.Line(m, players.get_current_player())
-        ngameState = gameState.test_move(
+        scored, ngameState = gameState.test_move(
             line)
-        (x, x,  v, max_move) = maximize(
-            ngameState,  players, alpha, beta)
+        if scored:
+            (x, x,  v, max_move) = minimize(
+                ngameState,  players, alpha, beta)
+        else:
+            players.switch_player()
+            (x, x,  v, max_move) = maximize(
+                ngameState,  players, alpha, beta)
+
         if v < min_value:
             min_value = v
             best_move = m
 
         if min_value <= alpha:
-            players.switch_player()
+            # players.switch_player()
             return (ngameState,  players, min_value, best_move)
 
         if min_value < beta:
             beta = min_value
 
-        players.switch_player()
+        # players.switch_player()
         return (ngameState, players, min_value, best_move)
 
 
